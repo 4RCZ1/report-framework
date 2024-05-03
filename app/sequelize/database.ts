9a5dadce081e6719974f2b3d6696c3714503abb6
4RCZ1 <marcinowicz1@gmail.com>
2024-05-04 00:48:06 +0200

    chore: removed css from the template next app, removed package-lock.json as this project aims to use yarn

--- app/sequelize/database.ts
+++ app/sequelize/database.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from 'sequelize';
+import * as pg from 'pg';
 
 const database = 'postgres';
 const username = 'postgres';
@@ -9,6 +10,7 @@ const dialect = 'postgres';
 export const sequelize = new Sequelize(database, username, password, {
   host,
   dialect,
+  dialectModule: pg,
   logging: true
 });
 
@@ -18,6 +20,6 @@ sequelize.authenticate()
   .catch((error: any) => console.error('Unable to connect to the database:', error));
 
 // Sync all models
-sequelize.sync({ force: true})
+sequelize.sync({ force: false})
   .then(() => console.log('All models were synchronized successfully.'))
   .catch(error => console.error('An error occurred while synchronizing models:', error));
